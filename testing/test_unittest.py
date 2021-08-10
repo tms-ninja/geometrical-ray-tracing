67b5d6a0680c9b55bb003caa204a432b8aa2796f
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-08-10 17:06:28 +0100

    Add basic property/methods tests for PyRefract_Sph

--- testing/test_unittest.py
+++ testing/test_unittest.py
@@ -359,5 +359,192 @@ class Test_PyMirror_Sph(unittest.TestCase, useful_checks):
     # TODO: add tests to verify correct ray tracing
 
 
+class Test_PyRefract_Sph(unittest.TestCase, useful_checks):
+    """Tests property access and methods of PyMirror_Plane"""
+    _centre = np.array([5.6, 7.8])
+    _R = 4.5
+    _start = 1.2
+    _end = 3.4
+    _n1 = 1.33
+    _n2 = 2.0
+    
+    def create_Obj(self):
+        """Creates an instance of PyMirror_Sph"""
+        return tr.PyRefract_Sph(self._centre, self._R, self._start, 
+                                self._end, self._n1, self._n2)
+
+    # TODO: Test __cinit__()
+
+    # Testing property centre
+    def test_centre_get(self):
+        """Tests property centre getting"""
+        m = self.create_Obj()
+
+        assert_array_equal(m.centre, self._centre)
+
+    def test_centre_set(self):
+        """Tests property centre setting"""
+        m = self.create_Obj()
+
+        self.check_np_view_shape_2_set(m, 'centre', self._centre, self._centre + 10.0)
+
+    def test_centre_set_none_not_allowed(self):
+        """Tests property centre cannot be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.centre = None
+
+    # Testing property R
+    def test_R_get(self):
+        """Tests property R getting"""
+        m = self.create_Obj()
+
+        self.assertEqual(m.R, self._R)
+
+    def test_R_set(self):
+        """Tests property R getting"""
+        m = self.create_Obj()
+
+        self.check_float_property(m, 'R', self._R, self._R + 10.0)
+
+    def test_R_Set_None_not_allowed(self):
+        """Tests property R cannot be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.R = None
+
+    # Testing property start
+    def test_start_get(self):
+        """Tests property start getting"""
+        m = self.create_Obj()
+
+        self.assertEqual(m.start, self._start)
+
+    def test_start_set(self):
+        """Tests property start setting"""
+        m = self.create_Obj()
+
+        self.check_float_property(m, 'start', self._start, self._end - 123.0)
+        
+    def test_start_set_none_not_allowed(self):
+        """Tests property start can't be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.start = None
+
+    def test_start_must_be_less_than_end(self):
+        """Tests when setting property start that it must be greater than property end"""
+        m = self.create_Obj()
+
+        # OK to set it something less than end
+        m.start = m.end - 1.0
+
+        # Raises exception if it's more
+        with self.assertRaises(AssertionError) as context:
+            m.start = m.end = 1.0
+
+    # Testing property end
+    def test_end_get(self):
+        """Tests property end getting"""
+        m = self.create_Obj()
+
+        self.assertEqual(m.end, self._end)
+
+    def test_end_set(self):
+        """Tests property end setting"""
+        m = self.create_Obj()
+
+        self.check_float_property(m, 'end', self._end, self._start + 123.0)
+
+    def test_end_set_none_not_allowed(self):
+        """Tests property end cannot be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.end = None
+
+    def test_end_must_be_greater_than_start(self):
+        """Tests when setting property start that it must be greater than property end"""
+        m = self.create_Obj()
+
+        # OK to set it something less than end
+        m.end = m.start + 1.0
+
+        # Raises exception when trying to set end less than start
+        with self.assertRaises(AssertionError) as context:
+            m.end = m.start - 1.0
+
+    # Test property n1 (first refractive index)
+    def test_n1_get(self):
+        """Tests property n1 getting"""
+        m = self.create_Obj()
+
+        self.assertEqual(m.n1, self._n1)
+
+    def test_n1_set(self):
+        """Tests property n1 getting"""
+        m = self.create_Obj()
+
+        self.check_float_property(m, 'n1', self._n1, self._n1 + 10.0)
+
+    def test_n1_Set_None_not_allowed(self):
+        """Tests property n1 cannot be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.n1 = None
+
+    # Test property n2 (second refractive index)
+    def test_n2_get(self):
+        """Tests property n2 getting"""
+        m = self.create_Obj()
+
+        self.assertEqual(m.n2, self._n2)
+        
+    def test_n2_set(self):
+        """Tests property n2 setting"""
+        m = self.create_Obj()
+
+        self.check_float_property(m, 'n2', self._n2, self._n2 + 10.0)
+
+    def test_n2_Set_None_not_allowed(self):
+        """Tests property n2 cannot be set to None"""
+        m = self.create_Obj()
+
+        with self.assertRaises(TypeError) as context:
+            m.n2 = None
+
+    # Test plot() method
+    def test_plot(self):
+        """Tests the plot method returns start point followed by end point"""
+        m = self.create_Obj()
+
+        def gen_plot_points(centre, R, start, end, n_points):
+            expected = np.empty((n_points, 2), dtype=np.double)
+            
+            t = np.linspace(start, end, n_points)
+            
+            expected[:, 0] = centre[0] + R*np.cos(t)
+            expected[:, 1] = centre[1] + R*np.sin(t)
+
+            return expected
+
+        # Test using default number of points of 100
+        expected = gen_plot_points(self._centre, self._R, self._start, self._end, n_points=100)
+        assert_array_equal(m.plot(), expected)
+
+        # Test using more points
+        expected = gen_plot_points(self._centre, self._R, self._start, self._end, n_points=155)
+        assert_array_equal(m.plot(n_points=155), expected)
+
+
+    # Testing correct ray tracing
+    # TODO: add tests to verify correct ray tracing
+
+
+
 if __name__ == '__main__':
     unittest.main()
