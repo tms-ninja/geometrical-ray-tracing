8590d92e65ccf0c3d87a25106fb218780110d204
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2021-08-30 20:38:54 +0100

    Add function for testing performance of Refract_Plane

--- cpp/ray-tracing.cpp
+++ cpp/ray-tracing.cpp
@@ -6,10 +6,11 @@
 #include <iomanip>
 #include <chrono>
 
-#include "optics\general.h"
-#include "optics\Mirror_Sph.h"
-#include "optics\Mirror_Plane.h"
+#include "optics/general.h"
+#include "optics/Mirror_Sph.h"
+#include "optics/Mirror_Plane.h"
 #include "optics/Refract_Sph.h"
+#include "optics/Refract_Plane.h"
 
 void test_Mirror_Sph()
 {
@@ -57,7 +58,6 @@ void test_Mirror_Plane()
 	{
 		std::vector<Ray> rays;
 		double theta{ M_PI / 180.0 * 60.0 };
-
 
 		rays.push_back(Ray({ 1.0, 0 }, { cos(theta), sin(theta) }));
 
@@ -113,12 +113,92 @@ void test_Refract_Sph()
 	std::cout << "Total duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000 << "[ms]" << std::endl;
 }
 
+void test_Refract_Plane()
+{
+	comp_list c;
+
+	// Mirrors so ray doesn't escape
+	add_component(c, Mirror_Plane({ -10.0, 10.0 }, { 10.0, 10.0 }));
+	add_component(c, Mirror_Plane({ 10.0, 10.0 }, { 10.0, -10.0 }));
+	add_component(c, Mirror_Plane({ 10.0, -10.0 }, { -10.0, -10.0 }));
+	add_component(c, Mirror_Plane({ -10.0, -10.0 }, { -10.0, 10.0 }));
+
+	double L_vals[] = { 5.0, 7.0, 9.0 };
+	double n_in[] = { 1.2, 1.3, 1.4 };
+	double n_out[] = { 1.0, 1.2, 1.3 };
+	double L;
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		L = L_vals[i];
+
+		add_component(c, Refract_Plane({ -L, L }, { L, L }, n_out[i], n_in[i]));
+		add_component(c, Refract_Plane({ L, L }, { L, -L }, n_out[i], n_in[i]));
+		add_component(c, Refract_Plane({ L, -L }, { -L, -L }, n_out[i], n_in[i]));
+		add_component(c, Refract_Plane({ -L, -L }, { -L, L }, n_out[i], n_in[i]));
+	}
+
+	auto begin = std::chrono::steady_clock::now();
+
+	// Define the list of rays and add
+	for (size_t i = 0; i < 70000; i++)
+	{
+		std::vector<Ray> rays;
+		double theta{ M_PI / 180.0 * 60.0 };
+
+		rays.push_back(Ray({ 1.0, 0 }, { cos(theta), sin(theta) }));
+
+		std::vector<Ray*> ray_ptrs;
+
+		for (auto &r : rays)
+			ray_ptrs.push_back(&r);
+
+		trace(c, ray_ptrs, 100);
+	}
+
+	auto end = std::chrono::steady_clock::now();
+
+	std::cout << "Total duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000 << "[ms]" << std::endl;
+
+}
+
+void test_concentric_Mirror_Sph()
+{
+	comp_list c;
+
+	add_component(c, Mirror_Sph({ 0.0, 0.0 }, 10.0, 0.0, 2 * M_PI));
+	add_component(c, Mirror_Sph({ 0.0, 0.0 }, 5.0, 0.0, 2 * M_PI));
+
+	auto begin = std::chrono::steady_clock::now();
+
+	// Define the list of rays and add
+	std::vector<Ray> rays;
+	double theta{ M_PI / 180.0 * 10.0 };
+
+
+	rays.push_back(Ray({ -8.0, 0 }, { cos(theta), sin(theta) }));
+
+	std::vector<Ray*> ray_ptrs;
+
+	for (auto &r : rays)
+		ray_ptrs.push_back(&r);
+
+	trace(c, ray_ptrs, 25);
+
+	auto end = std::chrono::steady_clock::now();
+
+	std::cout << "Total duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000 << "[ms]" << std::endl;
+
+	std::cout << rays[0];
+}
+
+
 
 int main()
 {
 	std::cout << std::fixed << "Program started!\n";
 
-	test_Refract_Sph();
+	test_Refract_Plane();
 
 	// Save rays and components
 	//std::cout << rays[0];
