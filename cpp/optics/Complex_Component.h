4f1851c9b39c1b648903da272544ccda9d25860f
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2022-06-01 16:38:03 +0100

    Add license headers

--- cpp/optics/Complex_Component.h
+++ cpp/optics/Complex_Component.h
@@ -1,3 +1,22 @@
+// geometrical-ray-tracing: Program to perform geometrical ray tracing
+// Copyright (C) 2022  Tom Spencer (tspencerprog@gmail.com)
+
+// This file is part of geometrical-ray-tracing
+
+// geometrical-ray-tracing is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 // Class to describe component composed of multiple sub-components
 // E.g. triangluar prism can be described as three planar boundaries where refraction occurs
 #pragma once
